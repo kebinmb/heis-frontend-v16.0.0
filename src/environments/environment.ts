1336eff873e60558e7e0f6393bc899236648e5cb
kevinmb1011 <114050643+kevinmb1011@users.noreply.github.com>
2024-08-12 22:27:22 +0800

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,12 @@
 export const environment = {
     production: false,
     apiBaseUrl: 'http://localhost:8080',
+    emailConfig: {
+      primaryUsername: 'johnkevin.moraca@chmsu.edu.ph',
+      primaryPassword: 'pbxa jpbh ojzn yjke',
+      secondaryUsername: 'jkmoraca.work@gmail.com',
+      secondaryPassword: 'imja bppr qkdl zjxe',
+      usageLimit: 100 // Example usage limit
+    }
   };
   
